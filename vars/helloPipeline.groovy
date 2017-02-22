c5caa2f7e892840ebaa57034404a382a8a4ab250
Bill Dennis <bill.dennis@gmail.com>
2017-02-22 20:27:22 +0000

    Experiment

---
+++ vars/helloPipeline.groovy
@@ -0,0 +1,19 @@
+// Can I load it from /vars?
+pipeline {
+
+    // All stages will run on any node
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+              echo "Hello"
+            }
+        }
+        stage('World') {
+            steps {
+              echo "World"
+            }
+        }        
+    }
+}
