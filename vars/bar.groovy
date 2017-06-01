83bb4325bbf43650cbc6b1902f3b1f15d3bc4806
Bill Dennis <bill.dennis@gmail.com>
2017-06-01 01:04:06 +0100

    Update bar.groovy

--- vars/bar.groovy
+++ vars/bar.groovy
@@ -1,7 +1,29 @@
+
+//
+// Something like:
+//
+// bar {
+//    script = {
+//        foo()
+//        return 'Called foo'
+//    }
+// }
+//
 def call(body) {
-    body.delegate = [:]
+    def config = [:]
+    body.delegate = config
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body()
     
+    // In the bar DSL element
     echo 'I am bar'
+    
+    // Expecting a script element as a closure
+    assert config.script != null, 'A script element was not supplied'
+    assert config.script instanceof Closure, 'The script element supplied must be a closure'
+    
+    // Call the script closure
+    config.script.delegate = this
+    config.script.resolveStrategy = Closure.DELEGATE_FIRST
+    def result = config.script.call()
 }
