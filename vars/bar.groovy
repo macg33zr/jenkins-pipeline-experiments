55d0a0a8c5adf0f93c3ccda7caade8f8d4e59d8f
Bill Dennis <bill.dennis@gmail.com>
2017-06-01 01:08:42 +0100

    Update bar.groovy

--- vars/bar.groovy
+++ vars/bar.groovy
@@ -19,8 +19,8 @@ def call(body) {
     echo 'I am bar'
     
     // Expecting a script element as a closure
-    assert config.script != null, 'A script element was not supplied'
-    assert config.script instanceof Closure, 'The script element supplied must be a closure'
+    //assert config.script != null, 'A script element was not supplied'
+    //assert config.script instanceof Closure, 'The script element supplied must be a closure'
     
     // Call the script closure
     config.script.delegate = this
